baceaf10190f199fc21e18437fc26f8eefe564d2
@Lili152645 <tonyarteestilo@gmail.com>
2023-01-29 20:15:58 +0000

    aula1

---
+++ Aula 1/src/main/java/org/aula1/Aula1.java
@@ -0,0 +1,9 @@
+package org.aula1;
+
+public class Aula1 {
+    public static void main(String[] args){
+        System.out.println("Sua força é Aprender");
+        System.out.println("Dar valor a vida");
+    }
+
+}
